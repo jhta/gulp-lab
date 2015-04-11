1bb34c1efa94079e07504dd35343a88c0f3b91c1
jhta <jeisonhs93@gmail.com>
2015-04-10 22:24:05 -0500

    first task

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,20 @@
 var gulp = require('gulp');
+
+var concat 	= require('gulp-concat');
+var uglify  = require('gulp-uglify');
+
+
+//DEFINE TASK
+//============
+//our first task
+gulp.task('js', function() {
+	//source files
+	gulp.src('js/**/*.*')
+	 //proccess
+	.pipe(concat('build.js'))
+	.pipe(uglify())
+	//files dest
+	.pipe(gulp.dest('dist/js'))
+});
+
+
